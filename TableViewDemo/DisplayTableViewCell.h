45fdc80c875f4505a91a4fd584000c4b60613814
Nidhi Patel <nidhi@creolestudios.com>
2017-10-27 17:18:00 +0530

    Add first

---
+++ TableViewDemo/DisplayTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  DisplayTableViewCell.h
+//  TableViewDemo
+//
+//  Created by Nidhi on 10/27/17.
+//  Copyright © 2017 CreoleStudios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DisplayTableViewCell : UITableViewCell
+
+@end
---
+++ TableViewDemo/DisplayTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  DisplayTableViewCell.m
+//  TableViewDemo
+//
+//  Created by Nidhi on 10/27/17.
+//  Copyright © 2017 CreoleStudios. All rights reserved.
+//
+
+#import "DisplayTableViewCell.h"
+
+@implementation DisplayTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
