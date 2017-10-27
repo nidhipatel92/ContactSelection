fc6b56485d87ff7fc05f2d52d44aaa97205e6090
Nidhi Patel <nidhi@creolestudios.com>
2017-10-27 15:28:34 +0530

    Initial Commit

---
+++ TableViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewDemo
+//
+//  Created by Nidhi on 10/27/17.
+//  Copyright © 2017 CreoleStudios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewDemo
+//
+//  Created by Nidhi on 10/27/17.
+//  Copyright © 2017 CreoleStudios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
